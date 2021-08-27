2d546cfd5b5ce4dc12596d1c5b9a68af7b9b42f3
ibrahim <dnzibrahim1@gmail.com>
2021-08-28 01:48:42 +0300

    Add schema validation with yup

--- simple-form/src/components/YoutubeForm.js
+++ simple-form/src/components/YoutubeForm.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useFormik } from "formik";
+import * as Yup from "yup";
 
 const initialValues = {
   name: "",
@@ -11,6 +12,7 @@ const onSubmit = (values) => {
   console.log("Form values", values);
 };
 
+/* 
 const validate = (values) => {
   let errors = {};
 
@@ -30,12 +32,20 @@ const validate = (values) => {
 
   return errors;
 };
+ */
+
+const validationSchema = Yup.object({
+  name: Yup.string().required("Required"),
+  email: Yup.string().email("Invalid email format").required("Required"),
+  channel: Yup.string().required("Required"),
+});
 
 function YoutubeForm() {
   const formik = useFormik({
     initialValues,
     onSubmit,
-    validate,
+    validationSchema,
+    //validate
   });
 
   return (
