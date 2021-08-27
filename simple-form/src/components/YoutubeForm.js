31c2689aef1b4670dc1072e15239a04713a2def7
ibrahim <dnzibrahim1@gmail.com>
2021-08-28 01:17:47 +0300

    Add validation function

--- simple-form/src/components/YoutubeForm.js
+++ simple-form/src/components/YoutubeForm.js
@@ -1,16 +1,41 @@
 import React from "react";
 import { useFormik } from "formik";
 
+const initialValues = {
+  name: "",
+  email: "",
+  channel: "",
+};
+
+const onSubmit = (values) => {
+  console.log("Form values", values);
+};
+
+const validate = (values) => {
+  let errors = {};
+
+  if (!values.name) {
+    errors.name = "Required";
+  }
+
+  if (!values.email) {
+    errors.email = "Required";
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = "Invalid email format";
+  }
+
+  if (!values.channel) {
+    errors.channel = "Required";
+  }
+
+  return errors;
+};
+
 function YoutubeForm() {
   const formik = useFormik({
-    initialValues: {
-      name: "",
-      email: "",
-      channel: "",
-    },
-    onSubmit: (values) => {
-      console.log("Form values", formik.values);
-    },
+    initialValues,
+    onSubmit,
+    validate,
   });
 
   return (
