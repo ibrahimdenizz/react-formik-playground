5cab275a799b672b3b38fff483b8a411e3fe3e81
ibrahim <dnzibrahim1@gmail.com>
2021-08-28 21:06:16 +0300

    Add enrollment form

---
+++ formik-controls/src/components/EnrollmentForm.js
@@ -0,0 +1,72 @@
+import React from "react";
+import { Form, Formik } from "formik";
+import * as Yup from "yup";
+import FormikControl from "./FormikControl";
+
+function EnrollmentForm() {
+  const dropdownOptions = [
+    { key: "Select and option", value: "" },
+    { key: "React", value: "react" },
+    { key: "Angular", value: "angular" },
+    { key: "Vue", value: "vue" },
+  ];
+
+  const checkboxOptions = [
+    { key: "HTML", value: "html" },
+    { key: "CSS", value: "css" },
+    { key: "JavaScript", value: "javascript" },
+  ];
+
+  const initialValues = {
+    email: "",
+    bio: "",
+    course: "",
+    skills: [],
+    courseDate: null,
+  };
+
+  const validationSchema = Yup.object({
+    email: Yup.string().email("Invalid email format").required("Required"),
+    bio: Yup.string().required("Required"),
+    course: Yup.string().required("Required"),
+    courseDate: Yup.date().required("Required").nullable(),
+  });
+
+  const onSubmit = (values) => console.log("Form data", values);
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={onSubmit}
+    >
+      {(formik) => (
+        <Form>
+          <FormikControl
+            control="input"
+            type="email"
+            label="Email"
+            name="email"
+          />
+          <FormikControl control="textarea" label="Bio" name="bio" />
+          <FormikControl
+            control="select"
+            label="Course"
+            name="course"
+            options={dropdownOptions}
+          />
+          <FormikControl
+            control="checkbox"
+            label="Your skill set"
+            name="skills"
+            options={checkboxOptions}
+          />
+          <FormikControl control="date" label="Course data" name="courseDate" />
+          <button type="submit">Submit</button>
+        </Form>
+      )}
+    </Formik>
+  );
+}
+
+export default EnrollmentForm;
