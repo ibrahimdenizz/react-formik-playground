09626a663b3b750dfcab0a5a740bcd4a22a77697
ibrahim <dnzibrahim1@gmail.com>
2021-08-28 14:05:25 +0300

    Add field revisited

--- simple-form/src/components/YoutubeForm.js
+++ simple-form/src/components/YoutubeForm.js
@@ -6,6 +6,8 @@ const initialValues = {
   name: "",
   email: "",
   channel: "",
+  comments: "",
+  address: "",
 };
 
 const onSubmit = (values) => {
@@ -40,8 +42,34 @@ function YoutubeForm() {
 
         <div className="form-control">
           <label htmlFor="channel">Channel</label>
-          <Field type="text" name="channel" id="channel" />
+          <Field
+            type="text"
+            name="channel"
+            id="channel"
+            placeholder="Youtube channel name"
+          />
           <ErrorMessage name="channel" />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="comments">Comments</label>
+          <Field as="textarea" id="comments" name="comments" />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="address">address</label>
+          <Field name="address">
+            {(props) => {
+              const { field, meta } = props;
+
+              return (
+                <div>
+                  <input type="text" id="address" {...field} />
+                  {meta.touched && meta.error ? <div>{meta.error}</div> : null}
+                </div>
+              );
+            }}
+          </Field>
         </div>
 
         <button type="submit">Submit</button>
